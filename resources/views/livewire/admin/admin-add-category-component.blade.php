df7a386ad4410a2b2cf28df4e3f02da7200d5643
Ilnur <ilnur.gaet@gmail.com>
2021-03-14 00:04:21 +0600

    replaced wire:keyup with wire:change.lazy in admin forms

--- resources/views/livewire/admin/admin-add-category-component.blade.php
+++ resources/views/livewire/admin/admin-add-category-component.blade.php
@@ -12,7 +12,7 @@
 
 
                 <label for="name" class="leading-7 text-sm text-gray-600">Category Name</label>
-                <input type="text" name="name" wire:model="name" wire:keyup="generateSlug"
+                <input type="text" name="name" wire:model="name" wire:change.lazy="generateSlug"
                        class="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
 
                 @error('name')
